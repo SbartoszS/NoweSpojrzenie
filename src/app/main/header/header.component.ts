7aecd111a70864966199a23f6e9cd3a307b7188d
BStojek <brulin@wp.pl>
2020-12-14 17:41:11 +0100

    add new icons, new script for nav effect, correct colors

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
+declare var $: any;
+let prevScrollpos: number = window.pageYOffset;
 @Component({
   selector: 'ns-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
-  ngOnInit(): void {
+  windowChangeSize: number = 992
+  openNav(event: any) {
+    if (window.innerWidth > this.windowChangeSize) { }
+    else {
+      $('.open-button').toggle();
+      $("body").toggleClass("fixed");
+      $('.mobile-menu').toggleClass('open');
+      $('.overlay').toggleClass('blurred');
+    }
   }
 
+  @HostListener('window:scroll', ['$event'])
+  onScrollNav($event) {
+    let headerHeight = $('header').innerHeight();
+    let nav = $('.navbar').innerHeight();
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > headerHeight + (nav - headerHeight)) {
+        $('.navbar').addClass('bottom-shadow');
+        $('.logo-nav').css({ "width": "60px", 'transition': '0.2s ease' });
+      }
+      else {
+        $('.navbar').removeClass('bottom-shadow');
+        $('.logo-nav').css('width', '100px');
+      }
+    });
+  }
 }
+

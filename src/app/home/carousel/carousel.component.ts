a23e8f6a1e8896c0abfffdc69beac54ad38e1aa1
Krzysiek <ksmiertka@gmail.com>
2020-12-15 19:44:30 +0100

    fix touch move
    fix touch move func

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -10,14 +10,23 @@ export class CarouselComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    $(document).ready(function() {
-      $("#carouselExampleIndicators").swiperight(function() {
-         $(this).carousel('prev');
-       });
-      $("#carouselExampleIndicators").swipeleft(function() {
-         $(this).carousel('next');
-      });
-   });
+  $('#carouselExampleIndicators').on('touchstart', function(event){
+    const xClick = event.originalEvent.touches[0].pageX;
+    $(this).one('touchmove', function(event){
+        const xMove = event.originalEvent.touches[0].pageX;
+        const sensitivityInPx = 5;
+
+        if( Math.floor(xClick - xMove) > sensitivityInPx ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
+            $(this).carousel('prev');
+        }
+    });
+    $(this).on('touchend', function(){
+        $(this).off('touchmove');
+    });
+});
   }
 
 }

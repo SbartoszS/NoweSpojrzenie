8fa404f28f41daf3a5d25d337c69808163608301
BStojek <brulin@wp.pl>
2020-12-17 00:08:24 +0100

    little change hove effect

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'ns-gallery',
   templateUrl: './gallery.component.html',
@@ -10,6 +9,7 @@ export class GalleryComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }

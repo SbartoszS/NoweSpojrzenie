b25fdf4efb3b7e987bf4a8aa79493f8f246cbcbd
BStojek <brulin@wp.pl>
2020-12-23 01:37:12 +0100

    new pricelist.json , added method in service

--- src/shared/gallery.service.ts
+++ src/shared/gallery.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 
 export interface Gallery {
@@ -8,11 +9,29 @@ export interface Gallery {
   imageUrl: string;
 }
 
+export interface PriceList {
+  id: number;
+  name: string,
+  price: number,
+  supplementPrice: number,
+  categories: string
+}
+
 @Injectable()
 export class GalleryService {
 
   constructor(private http: HttpClient) { }
+
   getAll(): Observable<Gallery[]> {
     return this.http.get<Gallery[]>('./assets/gallery.json');
+  }
+
+  getAllPrice(): Observable<PriceList[]> {
+    return this.http.get<PriceList[]>('./assets/price-list.json');
+  }
+
+  getByCategoryPrice(category: string): Observable<PriceList[]> {
+    return this.http.get<PriceList[]>('./assets/price-list.json').pipe(
+      map(products => products.filter(p => p.categories.includes(category))));
   }
 }

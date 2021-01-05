a0539be4200824909cb9acb9b26f70e757075e36
BStojek <brulin@wp.pl>
2021-01-05 17:04:01 +0100

    offer ts scripts

--- src/app/main-offers/main-offers.component.ts
+++ src/app/main-offers/main-offers.component.ts
@@ -3,17 +3,32 @@ import { Observable } from 'rxjs';
 import { HttpService } from 'src/shared';
 import { SingleOffer } from '../models/SingleOffer';
 
+declare var $: any;
+
 @Component({
   selector: 'ns-main-offers',
   templateUrl: './main-offers.component.html',
   styleUrls: ['./main-offers.component.scss']
 })
 export class MainOffersComponent implements OnInit {
-offersList : Observable<SingleOffer[]>;
-  constructor(private httpService : HttpService) { }
+  offersList: Observable<SingleOffer[]>;
+  constructor(private httpService: HttpService) { }
 
   ngOnInit(): void {
     this.offersList = this.httpService.getAllOffers();
+
+    $(".slider__input").on('input change', function (event: any) {
+      let sliderValue = $(this).val();
+      console.log(sliderValue);
+      $(".slider__drag").css("left", sliderValue + "%");
+      $(".comparision__img-2").css("width", sliderValue + "%");
+    })
+  }
+
+  dafaq(): void {
+    $(".single-offer").on('click', function () {
+      console.log("hmmm");
+    })
   }
 
 }

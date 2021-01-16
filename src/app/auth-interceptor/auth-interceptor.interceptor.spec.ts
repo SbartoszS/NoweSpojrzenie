c7ad9541f55979cf7cd942c7d03deb3dd11bf686
BStojek <brulin@wp.pl>
2021-01-16 23:29:03 +0100

    update prod version

--- src/app/auth-interceptor/auth-interceptor.interceptor.spec.ts
+++ src/app/auth-interceptor/auth-interceptor.interceptor.spec.ts
@@ -1,16 +1,16 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { AuthInterceptorInterceptor } from '../auth-interceptor/auth-interceptor.interceptor';
+import { AuthInterceptorInterceptor } from '../auth-interceptor/auth-interceptor.interceptor';
 
-// describe('AuthInterceptorInterceptor', () => {
-//   beforeEach(() => TestBed.configureTestingModule({
-//     providers: [
-//       AuthInterceptorInterceptor
-//       ]
-//   }));
+describe('AuthInterceptorInterceptor', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      AuthInterceptorInterceptor
+    ]
+  }));
 
-//   it('should be created', () => {
-//     const interceptor: AuthInterceptorInterceptor = TestBed.inject(AuthInterceptorInterceptor);
-//     expect(interceptor).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    const interceptor: AuthInterceptorInterceptor = TestBed.inject(AuthInterceptorInterceptor);
+    expect(interceptor).toBeTruthy();
+  });
+});

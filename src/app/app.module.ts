14a5c95aab8b5ce0641a53e78fb9820032807ec9
BStojek <brulin@wp.pl>
2021-01-17 00:37:40 +0100

    hashstrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { TrainingComponent } from './components/training/training.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { MainOffersComponent } from './components/main-offers/main-offers.component';
 import { AuthInterceptorInterceptor } from './auth-interceptor/auth-interceptor.interceptor'
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 
 @NgModule({
@@ -48,7 +49,11 @@ import { AuthInterceptorInterceptor } from './auth-interceptor/auth-interceptor.
     provide: HTTP_INTERCEPTORS,
     useClass: AuthInterceptorInterceptor,
     multi: true
-  }],
+  },
+    {
+      provide: LocationStrategy, useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

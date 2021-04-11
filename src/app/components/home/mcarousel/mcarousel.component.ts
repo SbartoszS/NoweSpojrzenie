f876ebbc3ef41b0cff0845896b67381b7c582ae9
Krzysiek <ksmiertka@gmail.com>
2021-04-11 21:31:35 +0200

    few fixes add local data json

--- src/app/components/home/mcarousel/mcarousel.component.ts
+++ src/app/components/home/mcarousel/mcarousel.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import Glide from '@glidejs/glide'
 @Component({
   selector: 'ns-mcarousel',
   templateUrl: './mcarousel.component.html',
   styleUrls: ['./mcarousel.component.scss']
 })
-export class McarouselComponent implements OnInit {
+export class McarouselComponent implements AfterViewInit {
 
   constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
 
     const config = {
       type: 'carousel',
@@ -28,7 +27,28 @@ export class McarouselComponent implements OnInit {
     }
 
     new Glide('.glide', config).mount()
-
   }
+
+  // ngOnInit(): void {
+
+  //   const config = {
+  //     type: 'carousel',
+  //     perView: '3',
+  //     gap: '15',
+  //     animationDuration: "1000",
+  //     autoplay: "4000",
+  //     breakpoints: {
+  //       576: {
+  //         perView: 1
+  //       },
+  //       992: {
+  //         perView: 2
+  //       },
+  //     }
+  //   }
+
+  //   new Glide('.glide', config).mount()
+
+  // }
 
 }

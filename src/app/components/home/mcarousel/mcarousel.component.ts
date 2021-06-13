219b9ba81b2989e9f0c5f4c9a007e33777917398
Krzysiek <ksmiertka@gmail.com>
2021-06-13 17:37:53 +0200

    remove consol logs and fix cert carousel

--- src/app/components/home/mcarousel/mcarousel.component.ts
+++ src/app/components/home/mcarousel/mcarousel.component.ts
@@ -1,31 +1,32 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import Glide from '@glidejs/glide'
+import Glide from '@glidejs/glide';
 @Component({
   selector: 'ns-mcarousel',
   templateUrl: './mcarousel.component.html',
-  styleUrls: ['./mcarousel.component.scss']
+  styleUrls: ['./mcarousel.component.scss'],
 })
 export class McarouselComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
     const config = {
       type: 'carousel',
       perView: '3',
       gap: '15',
-      animationDuration: "1000",
-      autoplay: "4000",
+      animationDuration: '1000',
+      autoplay: '4000',
       breakpoints: {
         576: {
-          perView: 1
+          perView: 1,
         },
         992: {
-          perView: 2
+          perView: 2,
         },
-      }
-    }
+      },
+    };
 
-    new Glide('.glide', config).mount()
+    window.addEventListener('load', function () {
+      new Glide('.glide', config).mount();
+    });
   }
+
+  ngOnInit(): void {}
 }
--- src/app/components/main-offers/main-offers.component.ts
+++ src/app/components/main-offers/main-offers.component.ts
@@ -10,14 +10,17 @@ declare var $: any;
 @Component({
   selector: 'ns-main-offers',
   templateUrl: './main-offers.component.html',
-  styleUrls: ['./main-offers.component.scss']
+  styleUrls: ['./main-offers.component.scss'],
 })
 export class MainOffersComponent implements OnInit {
   offersList: SingleOffer[];
   category;
   titlesList: OfferTitle;
-  constructor(private httpService: HttpService, private route: ActivatedRoute, private viewPortScroller: ViewportScroller) { }
-
+  constructor(
+    private httpService: HttpService,
+    private route: ActivatedRoute,
+    private viewPortScroller: ViewportScroller
+  ) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
@@ -25,41 +28,42 @@ export class MainOffersComponent implements OnInit {
       this.offersList = null;
       this.titlesList = null;
 
-      this.httpService.getAllOffersByCategory(this.category).subscribe(data => {
-        this.offersList = data;   
-        console.log(this.offersList);
-             
-      });
+      this.httpService
+        .getAllOffersByCategory(this.category)
+        .subscribe((data) => {
+          this.offersList = data;
+        });
 
-      this.httpService.getAllOffersTitleByCategory(this.category).subscribe(data => {
-        this.titlesList = data[0];
-      });
-    })
-
+      this.httpService
+        .getAllOffersTitleByCategory(this.category)
+        .subscribe((data) => {
+          this.titlesList = data[0];
+        });
+    });
   }
 
   switcharrow(name) {
     const nameS = `a${name}`;
     const arr1 = document.querySelector(`.${nameS}`);
-    arr1.classList.toggle("move");
+    arr1.classList.toggle('move');
   }
 
   progressChange(e: any): void {
     const input = e.target;
     const key = $(input).data('slide');
     let sliderValue = $(input).val();
-    $(`.${key} .slider__drag`).css("left", sliderValue + "%");
-    $(`.wrapper_${key} .comparision__img-2`).css("width", sliderValue + "%");
+    $(`.${key} .slider__drag`).css('left', sliderValue + '%');
+    $(`.wrapper_${key} .comparision__img-2`).css('width', sliderValue + '%');
   }
 
   smoothScroll(id) {
     let destinationId = `#acc${id}`;
-    console.log(destinationId);
-    
     const toTop = 120;
-    $('html, body').animate({
-      scrollTop: $(destinationId).offset().top - toTop
-    }, 1000)
+    $('html, body').animate(
+      {
+        scrollTop: $(destinationId).offset().top - toTop,
+      },
+      1000
+    );
   }
-
 }
